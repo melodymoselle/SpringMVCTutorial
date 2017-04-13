d20ba5441055667ebc1d7193e292a73fb258aa75
Melmo <melodymoselle@gmail.com>
2017-04-12 17:14:27 -0700

    Chapter 4: Controllers, Models, Data binding

--- src/main/java/com/melodymoselle/mvc/controllers/HomeController.java
+++ src/main/java/com/melodymoselle/mvc/controllers/HomeController.java
@@ -1,13 +1,24 @@
 package com.melodymoselle.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.melodymoselle.mvc.data.entities.Project;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/home")
-	public String goHome(){
+	public String goHome(Model model){
+		
+		Project project = new Project();
+		project.setName("First Project");
+		project.setSponsor("Nasa");
+		project.setDescription("This is a simple project sponsored by Nasa.");
+		
+		model.addAttribute("currentProject", project);
+		
 		return "home";
 	}
 }
